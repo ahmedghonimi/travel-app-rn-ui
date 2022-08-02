52259a890a4d4a94a52003b68e15907c78a1718b
ahmed.ali.mahmoud <ahmed.ali.mahmoud@accenture.com>
2022-08-02 15:45:44 +0300

    adding custom fonts and project archeticht

---
+++ TravelAppUI/src/constants/theme.js
@@ -0,0 +1,8 @@
+export const FONTS = {
+  GORDITA_BLACK: 'Gordita-Black',
+  GORDITA_BOLD: 'Gordita-Bold',
+  GORDITA_LIGHT: 'Gordita-Light',
+  GORDITA_MEDIUM: 'Gordita-Medium',
+  GORDITA_REGULAR: 'Gordita-Regular',
+  GORDITA_THIN: 'Gordita-Thin',
+};
